61b8c166e3572cb9bfa573eb2ac7afe1ae0f7a5c
Shohei Tanaka <st.ryoma@gmail.com>
2025-03-03 10:04:55 +0900

    Update Fixed bugs

--- includes/admin/class-jp4wc-admin-notices.php
+++ includes/admin/class-jp4wc-admin-notices.php
@@ -122,7 +122,12 @@ class JP4WC_Admin_Notices {
 					echo '<h2 style="color: #2C045D;">' . esc_html( $catch_copy ) . '</h2>';
 					esc_html_e( 'Coming into force in April 2025! Is your website ready for the new credit card payment standards?', 'woocommerce-for-japan' );
 					echo '<br />';
-					esc_html_e( 'We recommend that you take the following measures to ensure that your store is ready for the new credit card payment standards.', 'woocommerce-for-japan' );
+					esc_html_e( 'If you are not ready yet, please take the necessary measures as soon as possible.', 'woocommerce-for-japan' );
+					echo '<br />';
+					esc_html_e( 'Do you think all you need is EMV 3D Secure?', 'woocommerce-for-japan' );
+					esc_html_e( 'No, that\'s not enough. You need to take additional measures.', 'woocommerce-for-japan' );
+					echo '<br />';
+					esc_html_e( 'Please check the following link for more information.', 'woocommerce-for-japan' );
 					echo '<br />';
 					/* translators: 1: Opening anchor tag with URL, 2: Closing anchor tag */
 					$product_link = sprintf( __( '%1$s [Security measures for WooCommerce] in line with the "Credit Card Security Guidelines" %2$s', 'woocommerce-for-japan' ), '<a href="' . esc_url( $pr_link ) . '?utm_source=jp4wc_plugin&utm_medium=site&utm_campaign=woo_security" target="_blank">', '</a>' );
@@ -140,7 +145,13 @@ class JP4WC_Admin_Notices {
 					echo '<h2 style="color: #2C045D;">' . esc_html( $catch_copy ) . '</h2>';
 					esc_html_e( 'Are you prepared for the new security guidelines that will come into effect in April 2025?', 'woocommerce-for-japan' );
 					echo '<br />';
-					echo '<b>' . esc_html( 'Don\'t worry if you\'re not ready yet!', 'woocommerce-for-japan' ) . '</b> ' . esc_html( 'Let our experts help you establish a safe payment environment.', 'woocommerce-for-japan' );
+					echo '<b>' . esc_html__( 'Don\'t worry if you\'re not ready yet!', 'woocommerce-for-japan' ) . '</b> ';
+					esc_html_e( 'Let our experts help you establish a safe payment environment.', 'woocommerce-for-japan' );
+					echo '<br />';
+					esc_html_e( 'Do you think all you need is EMV 3D Secure?', 'woocommerce-for-japan' );
+					esc_html_e( 'No, that\'s not enough. You need to take additional measures.', 'woocommerce-for-japan' );
+					echo '<br />';
+					esc_html_e( 'Please check the following link for more information.', 'woocommerce-for-japan' );
 					echo '<br />';
 					/* translators: 1: Opening anchor tag with URL, 2: Closing anchor tag */
 					$product_link = sprintf( __( '%1$s 📢 Check it out now and be fully prepared! %2$s', 'woocommerce-for-japan' ), '<a href="' . esc_url( $pr_link ) . '?utm_source=jp4wc_plugin&utm_medium=site&utm_campaign=woo_security_safe" target="_blank">', '</a>' );
