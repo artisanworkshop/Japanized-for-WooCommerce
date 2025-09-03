51da1463990026af851ee438a6c525e328de19ec
Shohei Tanaka <st.ryoma@gmail.com>
2025-09-03 12:47:58 +0900

    Update 2.7.0

--- woocommerce-for-japan.php
+++ woocommerce-for-japan.php
@@ -8,10 +8,10 @@
  * Version: 2.7.0
  * Requires PHP: 8.1
  * Requires Plugins: woocommerce
- * Requires at least: 6.0
- * Tested up to: 6.8.1
+ * Requires at least: 6.7
+ * Tested up to: 6.8.2
  * WC requires at least: 8.0
- * WC tested up to: 9.8.5
+ * WC tested up to: 10.1.2
  *
  * Text Domain: woocommerce-for-japan
  * Domain Path: /i18n/
