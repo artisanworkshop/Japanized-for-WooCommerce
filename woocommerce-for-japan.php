2a8ca071a96e6c145abd9ae6c1ac336046c8eb12
Shohei Tanaka <st.ryoma@gmail.com>
2025-11-04 12:37:59 +0900

    Update 2.7.13

--- woocommerce-for-japan.php
+++ woocommerce-for-japan.php
@@ -5,13 +5,13 @@
  * Description: Woocommerce toolkit for Japanese use.
  * Author: Artisan Workshop
  * Author URI: https://wc.artws.info/
- * Version: 2.7.12
+ * Version: 2.7.13
  * Requires PHP: 8.1
  * Requires Plugins: woocommerce
  * Requires at least: 6.7
  * Tested up to: 6.8.3
  * WC requires at least: 8.0
- * WC tested up to: 10.2.2
+ * WC tested up to: 10.3.4
  *
  * Text Domain: woocommerce-for-japan
  * Domain Path: /i18n/
@@ -25,7 +25,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'JP4WC_VERSION', '2.7.12' );
+define( 'JP4WC_VERSION', '2.7.13' );
 
 require_once __DIR__ . '/class-jp4wc.php';
 
