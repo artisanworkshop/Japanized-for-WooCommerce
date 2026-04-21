1149462062682a460cd221c963f063c7adad5ae2
Shohei Tanaka <st.ryoma@gmail.com>
2026-04-22 07:05:29 +0900

    Add email context check to Yomigana field display logic

--- includes/class-jp4wc-address-fields.php
+++ includes/class-jp4wc-address-fields.php
@@ -247,7 +247,7 @@ class JP4WC_Address_Fields {
 		$include_yomigana = get_option( 'wc4jp-yomigana' );
 
 		if ( $has_block_checkout && ! is_order_received_page() ) {
-			if ( $include_yomigana && is_checkout() ) {
+			if ( $include_yomigana && is_checkout() && ! $this->is_email_context() ) {
 				$fields['JP'] = $fields['JP'] . __( '(Please click "Edit" to check the pronunciation.)', 'woocommerce-for-japan' );
 			}
 			return $fields;
@@ -413,6 +413,35 @@ class JP4WC_Address_Fields {
 			$address['phone']               = $args->get_meta( $order_id, '_shipping_phone', true );
 		}
 		return $address;
+	}
+
+	/**
+	 * Check if we are in an email sending context.
+	 *
+	 * @return bool True if in email context, false otherwise.
+	 */
+	private function is_email_context() {
+		$email_actions = array(
+			'woocommerce_email_header',
+			'woocommerce_email_footer',
+			'woocommerce_email_order_details',
+			'woocommerce_email_order_meta',
+			'woocommerce_email_customer_details',
+		);
+		foreach ( $email_actions as $action ) {
+			if ( doing_action( $action ) ) {
+				return true;
+			}
+		}
+		if ( function_exists( 'debug_backtrace' ) ) {
+			$backtrace = debug_backtrace( DEBUG_BACKTRACE_IGNORE_ARGS, 20 );
+			foreach ( $backtrace as $trace ) {
+				if ( isset( $trace['class'] ) && false !== strpos( $trace['class'], 'WC_Email' ) ) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 	/**
